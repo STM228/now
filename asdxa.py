33e490a28c07d0915d57906a6adb71b2f7188b40
STM228 <112960696+STM228@users.noreply.github.com>
2022-11-25 17:45:20 +0300

    Add files via upload

--- asdxa.py
+++ asdxa.py
@@ -26,12 +26,12 @@ def make_hero(
     if not name:
         name = f"{choice(first_name)} {choice(last_name)}"
 
-        hp_max = hp_now
-
-        xp_next = lvl * 1000
 
     if not hp_now:
         hp_now = randint(1, 100)
+
+    hp_max = hp_now
+    xp_next = lvl * 1000
 
     if money is None:
         money = randint(1, 100)
@@ -58,20 +58,21 @@ def make_hero(
 
     """
 
-    return [name,
-            hp_max,
-            hp_now,
-            lvl,
-            xp_next,
-            xp_now,
-            attack,
-            defence,
-            weapon,
-            shield,
-            luck,
-            money,
-            inventary
-        ]
+    return [
+        name,
+        hp_max,
+        hp_now,
+        lvl,
+        xp_next,
+        xp_now,
+        attack,
+        defence,
+        weapon,
+        shield,
+        luck,
+        money,
+        inventary
+    ]
 
 
 def show_hero(hero: str) -> None:
@@ -90,46 +91,69 @@ def show_hero(hero: str) -> None:
     print("")
 
 def levelup(hero):
-    if hero[5] >= hero[4]:
+    while hero[5] >= hero[4]:
         hero[3] += 1
         hero[4] = hero[3] * 1000
-        print(f"{hero[0]} получил {hero[3]} уровень, для следующего уровня тебе надо {hero[4]}\n")
+        print(f"{hero[0]} получил {hero[3]} уровень\n")
 
 
-def buy_item(hero: list, price: int) -> None:
-    """
-    Герой это список
-    [0] name - имя персонажа
-    [11] money - деньги
-    [12] inventary - инвентарь список
+def buy_item(hero: list, item, price: int) -> None:
+    """проверяет есть ли price денег у героя,
+     и если есть отнимает из денег героя price,
+     и добавляет в инвентарь героя этот item
     """
     if hero[11] >= price:
         hero[11] -= price
-        hero[12].append("зелье")
+        hero[12].append(item)
         print(f"{hero[0]} купил зелье за {price} монет")
     else:
-        print(f"{hero[0]} не хватило {price - hero[11]} монет")
+        print(f"{hero[0]} не хватило {price - hero[11]} монет ")
 
-os.system("cls")
-p1 = make_hero()
-show_hero(p1)
-buy_item(p1, 100)
-show_hero(p1)
 
-def consume_item(hero: list, idx):
-    """
-    TODO:  ВЫПИТЬ ЗЕЛЬЕ ТАК ЧТОБЫ БЫЛО НЕ БОЛЬШЕ МАКСИМУМА
-
-    """
+def consume_item(hero: list, idx: int) -> None:
     if idx <= len(hero[12]) - 1 and idx > -1:
         print(f"{hero[0]} употребил {hero[12][idx]}")
-
         if hero[12][idx] == "зелье":
-            pass
+            hero[2] += 10
+            if hero[2] > hero[1]:
+                hero[2] = hero[1]
         elif hero[12][idx] == "яблоко":
             pass
         else:
-            pass
+            print("употребилчто то не известное")
         hero[12].pop(idx)
     else:
         print("нет такого")
+
+
+def play_dise(hero: list, bet: int) -> None:
+    if bet > 0:
+        if hero[11] >= bet:
+            hero_score = randint(2, 12)
+            casino_score = randint(2, 12)
+            print(f"{hero[0]} выбросил {hero_score}")
+            print(f"Трактирщик выбросил {casino_score}")
+            if hero_score > casino_score:
+                hero[11] += bet
+                print(f"{hero[0]} выйграл и забирает {bet} монет")
+            elif hero_score < casino_score:
+                hero[11] -= bet
+                print(f"{hero[0]} проиграл {bet} монет")
+            else:
+                print("ничья")
+        else:
+            print(f"У {hero[0]} нет столько монет")
+    else:
+        print("такая ставка невозможна, минимальная ставка начинается от 1 монеты")
+
+
+def start_fight(hero: list) -> None:
+    """
+    TODO:
+    нужен противник
+    обмен ударами пока игрок и противник живы
+    итог боя: проигрыш или победа
+    победа: добавить опыт от врага, забрать предметы от врага в инвентарь героя
+    проигрыш: закончить игру
+    """
+    pass
